91a5aca4d2f0c6561b205dbe7c0df330bd100ff0
JzueRomero <rj.josueromero@gmail.com>
2019-10-03 09:08:14 -0600

    creacion de modelo y controller de productos

---
+++ ventasmvc/ventasmvc/Context/StoreContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+using ventasmvc.Models;
+
+namespace ventasmvc.Context
+{
+    public class StoreContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ ventasmvc/ventasmvc/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ventasmvc.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public DateTime LastBuy { get; set; }
+
+        public float Stock { get; set; }
+    }
+}
