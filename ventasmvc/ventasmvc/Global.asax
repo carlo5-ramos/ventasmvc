f62e96764e1da1aa5c40311af690f8f156e191aa
JzueRomero <rj.josueromero@gmail.com>
2019-10-03 08:48:17 -0600

    creacion de proyecto

---
+++ ventasmvc/ventasmvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ventasmvc.MvcApplication" Language="C#" %>
---
+++ ventasmvc/ventasmvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ventasmvc.Startup))]
+namespace ventasmvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
